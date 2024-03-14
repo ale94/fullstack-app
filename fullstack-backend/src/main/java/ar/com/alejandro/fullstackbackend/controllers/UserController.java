94e51cb7fb7d2d249bcdf87c415463fbb949f967
ale94 <alejandrorua111@gmail.com>
2024-03-13 23:27:11 -0300

    Se creo metodo HTTP GET users

--- fullstack-backend/src/main/java/ar/com/alejandro/fullstackbackend/controllers/UserController.java
+++ fullstack-backend/src/main/java/ar/com/alejandro/fullstackbackend/controllers/UserController.java
@@ -3,9 +3,12 @@ package ar.com.alejandro.fullstackbackend.controllers;
 import ar.com.alejandro.fullstackbackend.models.UserEntity;
 import ar.com.alejandro.fullstackbackend.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +19,10 @@ public class UserController {
     @PostMapping("/users")
     UserEntity newUser(@RequestBody UserEntity user) {
         return repository.save(user);
+    }
+
+    @GetMapping("/users")
+    List<UserEntity> getAllUsers() {
+        return repository.findAll();
     }
 }
