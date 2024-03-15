bf96ffeeebd74ae25d4a4916fc7505b0b7af6dbf
ale94 <alejandrorua111@gmail.com>
2024-03-14 23:24:13 -0300

    Se implemento Axios metodo get

--- fullstack-front/src/pages/Home.jsx
+++ fullstack-front/src/pages/Home.jsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8080/users");
+    setUsers(result.data);
+  };
+
   return (
     <div className="container">
       <div className="py-4">
@@ -6,29 +20,27 @@ export const Home = () => {
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colSpan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
+            {users.map((user, index) => {
+              return (
+                <>
+                  <tr>
+                    <th scope="row" key={index}>
+                      {index + 1}
+                    </th>
+                    <td>{user.name}</td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                  </tr>
+                </>
+              );
+            })}
           </tbody>
         </table>
       </div>
